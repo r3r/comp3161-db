122ec07bbc42048420bc9770f6676be190755cd5
Ritesh Reddy <ritesh_r_reddy@hotmail.com>
2015-04-01 16:01:42 -0500

    Fixed authenticate wrapper error.

--- flask_base.py
+++ flask_base.py
@@ -1,10 +1,13 @@
 __author__ = 'RiteshReddy'
-import __setup_path
-from flask import Flask, render_template, request, session, redirect, url_for
+from functools import wraps
+
+from flask import Flask, request, session, redirect, url_for
+
 app = Flask(__name__)
 app.secret_key = '\xa4\x1e\x8d\x1eC\x91{\xbd\x1c\x00\xd4H\xcaXC1\xdan\xbfa\xc3\xcb\x0eiXF\xd5#\xed\x9fg\xed'
 
 def authenticate(f):
+    @wraps(f)
     def wrapper(*args, **kwargs):
         if 'username' in session:
             return f(*args, **kwargs)
