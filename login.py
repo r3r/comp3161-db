4739052cfd297b96918f811edc6fc426c8c5c4f4
Ritesh Reddy <ritesh_r_reddy@hotmail.com>
2015-04-03 21:17:13 -0500

    Refactored to branch1

--- login.py
+++ login.py
@@ -4,30 +4,7 @@ from db import *
 from flask_base import *
 
 
-laptops = [  # fake array of laptops
-        {
-            'id': 1,
-            'name': 'Dell Inspiron',
-            'price': 45,
-            'description': 'Description of dell inspiron'
-        },
-        {
-            'id': 2,
-            'name': 'Hp note',
-            'price': 60,
-            'description': 'Decription of hp note'
-        },
-        {
-            'id': 3,
-            'name': 'Apple Macintosh',
-            'price': 600,
-            'description': 'Decription of mac'
-        }
-    ]
 
-branch1 = {
-            'name': 'Branch 1'
-        }
 
 
 @app.route('/', endpoint="index")
@@ -66,30 +43,10 @@ def signup():
     # save user to database and sign them in
 
 
-@app.route("/branch1")
-@authenticate
-def home():
-    return render_template("branch1.html", branch=branch1, laptops=laptops)
-
-
-@app.route("/item/<identifier>")
-@authenticate
-def item(identifier=None):
-    laptop = None
-
-    if identifier:
-        laptop = get_laptop(identifier)  #get laptop based on id
-
-    return render_template("item.html", identifier=identifier, laptop=laptop)
-
 
 def check_user(username, password):
     return username == password
 
 
-def get_laptop(identifier):
-    for laptop in laptops:
-        if int(identifier) == laptop.get('id'):
-            return laptop
-    return None
+
 
--- webserver.py
+++ webserver.py
@@ -2,7 +2,7 @@ __author__ = 'RiteshReddy'
 from flask_base import *
 from db import *
 from login import *
-
+from branch1 import *
 
 if __name__ == '__main__':
     app.run(debug=True)
